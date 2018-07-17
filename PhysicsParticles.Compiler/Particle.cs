95c01cdb6df94090857ab3b60af6f0be5c62f663
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2018-07-17 20:06:08 +0100

    Began making the compiler

---
+++ PhysicsParticles.Compiler/Particle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsParticles.Compiler
+{
+    public class Particle
+    {
+        public string Reference { get; set; }
+        public string Title { get; set; }
+        public string Symbol { get; set; }
+        public IList<string> Classes { get; set; }
+
+    }
+}
