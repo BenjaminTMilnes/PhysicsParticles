f2feba29c080696fdf6125d108994496177879e4
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2019-08-10 00:50:52 +0100

    Added more to compiler

--- compiler/compile.py
+++ compiler/compile.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 class Compiler (object):
     def __init__(self):
@@ -19,14 +20,85 @@ class Compiler (object):
             lines = fileObject.readlines()
             lines = [l.strip() for l in lines if l.strip() != ""]
 
-            return lines[0]
+            particle = {}
+
+            particle["Reference"] = lines[0]
+            particle["URLReference"] = particle["Reference"].lower()
+            particle["Name"] = lines[1]
+            particle["Symbol"] = lines[2]
+            particle["Classes"] = [c.strip() for c in  lines[3].split(",")]
+
+            lines = lines[4:]
+
+            for line in lines:                
+                if line.startswith("mass:"):
+                    particle["Mass"] = line[5:].strip()
+
+                if line.startswith("relative charge:"):
+                    particle["RelativeCharge"] = line[16:].strip()
+                    
+                if line.startswith("spin:"):
+                    particle["Spin"] = line[5:].strip()
+            
+                if line.startswith("magnetic moment:"):
+                    particle["MagneticMoment"] = line[16:].strip()
+        
+                if line.startswith("antiparticle:"):
+                    particle["Antiparticle"] = line[13:].strip()
+    
+                if line.startswith("mean lifetime:"):
+                    particle["MeanLifetime"] = line[14:].strip()
+
+                if line.startswith("year discovered:"):
+                    particle["YearDiscovered"] = line[16:].strip()
+
+                if line.startswith("discovered by:"):
+                    particle["DiscoveredBy"] = line[14:].strip()
+                    
+                if line.startswith("wikipedia:"):
+                    particle["WikipediaURL"] = line[10:].strip()
+
+            return particle
+
+    def removeParticlePages(self):
+
+        if not os.path.isdir("../web/particles"):
+            os.mkdir("../web/particles")
+
+        for a in os.listdir("../web/particles"):
+            os.remove(os.path.join("../web/particles", a))
+
+    def makeParticlePage(self, template, particle):
+
+        page = template
+
+        page = page.replace("[Name]", particle["Name"])
+
+        with open(os.path.join("../web/particles", particle["URLReference"]), "w") as fileObject:
+            fileObject.write(page)
 
     def compile(self):
 
         particleFilePaths = self.getParticleFilePaths()
 
-        for p in particleFilePaths:
-            print(self.compileParticle(p))
+        particles = [self.compileParticle(filePath) for filePath in particleFilePaths]
+
+        data = {}
+
+        data["Particles"] = particles
+
+        print(data)
+
+        with open("../data/Compiled.json", "w") as  fileObject:
+            json.dump(data, fileObject, indent = 4)
+
+        self.removeParticlePages()
+
+        with open("../web-templates/particle.html", "r") as fileObject:
+            particlePageTemplate = fileObject.read()
+
+        for particle in particles:
+            self.makeParticlePage(particlePageTemplate, particle)
 
 if __name__ == "__main__":
     compiler = Compiler()
