85cd4ff7695a9e01496e85e3f3eda5b03800e126
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2019-08-10 15:56:27 +0100

    Added more particles and more features

--- compiler/compile.py
+++ compiler/compile.py
@@ -1,5 +1,30 @@
 import os
 import json
+import re
+
+class MeasuredValue (object):
+    def __init__(self, significand = 0, exponent = 0, units = ""):
+        self.significand = significand
+        self.exponent = exponent
+        self.units = units
+
+    @staticmethod
+    def fromText(text):
+        match = re.match("([+-]?[\d\.]+)\s*\\times\s*10\^\{([+-]?\d+)\}\s*(kg|s)")
+
+        if match:
+            value = MeasuredValue()
+
+            value.significand = float(match.group(1))
+            value.exponent = float(match.group(2))
+            value.units = match.group(3)
+
+            return value
+
+        return None
+
+    def toHTML(self):
+        pass
 
 class Compiler (object):
     def __init__(self):
@@ -24,6 +49,7 @@ class Compiler (object):
 
             particle["Reference"] = lines[0]
             particle["URLReference"] = particle["Reference"].lower()
+            particle["URL"] = particle["URLReference"] + ".html"
             particle["Name"] = lines[1]
             particle["Symbol"] = lines[2]
             particle["Classes"] = [c.strip() for c in  lines[3].split(",")]
@@ -44,7 +70,7 @@ class Compiler (object):
                     particle["MagneticMoment"] = line[16:].strip()
         
                 if line.startswith("antiparticle:"):
-                    particle["Antiparticle"] = line[13:].strip()
+                    particle["AntiparticleReference"] = line[13:].strip()
     
                 if line.startswith("mean lifetime:"):
                     particle["MeanLifetime"] = line[14:].strip()
@@ -68,17 +94,33 @@ class Compiler (object):
         for a in os.listdir("../web/particles"):
             os.remove(os.path.join("../web/particles", a))
 
+    def convertLaTeXToHTML(self, latex):
+
+        html = latex
+        html = re.sub(r"\\times", "&times;", html)
+        html = re.sub(r"-(\d)", r"&minus;\g<1>", html)
+        html = re.sub(r"\\mu", r"&mu;", html)
+        html = re.sub(r"\^\{(.+)\}", r"<sup>\g<1></sup>", html)
+        html = re.sub(r"_\{(.+)\}", r"<sub>\g<1></sub>", html)
+
+        return html
+
     def makeParticlePage(self, template, particle):
 
         page = template
 
+        page = page.replace("[Reference]", particle["Reference"])
         page = page.replace("[Name]", particle["Name"])
         page = page.replace("[Symbol]", particle["Symbol"])
-        page = page.replace("[Mass]", particle["Mass"])
-        page = page.replace("[RelativeCharge]", particle["RelativeCharge"])
+        page = page.replace("[Mass]", self.convertLaTeXToHTML( particle["Mass"]))
+        page = page.replace("[RelativeCharge]", self.convertLaTeXToHTML( particle["RelativeCharge"]))
         page = page.replace("[Spin]", particle["Spin"])
-        page = page.replace("[MagneticMoment]", particle["MagneticMoment"])
-        page = page.replace("[MeanLifetime]", particle["MeanLifetime"])
+        page = page.replace("[MagneticMoment]", self.convertLaTeXToHTML( particle["MagneticMoment"]))
+        page = page.replace("[MeanLifetime]", self.convertLaTeXToHTML( particle["MeanLifetime"]))
+        page = page.replace("[WikipediaURL]", particle["WikipediaURL"])
+
+        page = page.replace("[Antiparticle_Name]", particle["Antiparticle"]["Name"])
+        page = page.replace("[Antiparticle_URL]", particle["Antiparticle"]["URL"])
 
         with open(os.path.join("../web/particles", particle["URLReference"] + ".html"), "w") as fileObject:
             fileObject.write(page)
@@ -88,6 +130,19 @@ class Compiler (object):
         particleFilePaths = self.getParticleFilePaths()
 
         particles = [self.compileParticle(filePath) for filePath in particleFilePaths]
+
+        for particle in particles:
+            antiparticles = [p for p in particles if p["Reference"] == particle["AntiparticleReference"]]
+
+            if len(antiparticles) > 0:
+                particle["Antiparticle"] = {}
+                particle["Antiparticle"]["Name"] = antiparticles[0]["Name"]
+                particle["Antiparticle"]["URL"] = antiparticles[0]["URL"]
+            else:
+                particle["Antiparticle"] = {}
+                particle["Antiparticle"]["Name"] =""
+                particle["Antiparticle"]["URL"] = ""
+
 
         data = {}
 
