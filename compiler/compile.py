393af3b9be31b0a466b13e25d1cf97df8f553ea7
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2020-02-14 16:20:20 +0000

    Various simplifications

--- compiler/compile.py
+++ compiler/compile.py
@@ -7,19 +7,50 @@ from decimal import *
 context = getcontext()
 context.prec = 30
 
+
+zero = Decimal("0")
+e1 = Decimal("10")
+e3 = Decimal("10") ** Decimal("3")
+e6 = Decimal("10") ** Decimal("6")
+e9 = Decimal("10") ** Decimal("9")
+e12 = Decimal("10") ** Decimal("12")
+evcc = Decimal("1.78266192") * Decimal("10") ** Decimal("-36")
+
+
+def orderOfMagnitude(n):
+    return int(math.floor(float(n.log10())))
+
+
+def powerOf10(e):
+    return e1 ** Decimal(e)
+
+
+def roundToNSF(n, nsf=3):
+    if n == zero or nsf == 0:
+        return n
+
+    o = orderOfMagnitude(n)
+    p = -o + nsf - 1
+
+    m = n * powerOf10(p)
+    m = round(m, 0)
+    m = m * powerOf10(-p)
+
+    return m
+
+
 class Measurement (object):
-    def __init__(self):
-        self.significand = Decimal(0)
-        self.exponent = Decimal(0)
-        self.unit = ""
+    def __init__(self, significand, exponent, unit):
+        self.significand = significand
+        self.exponent = exponent
+        self.unit = unit
 
-    def roundSignificand(self, nsf = 3):
-        return  str(roundToNSF( self.significand, nsf))
+    def roundSignificand(self, nsf=3):
+        return str(roundToNSF(self.significand, nsf))
 
-
-    def toHTML(self, nsf = 3):
+    def toHTML(self, nsf=3):
         s = self.roundSignificand(nsf)
-        e = str( self.exponent)
+        e = str(self.exponent)
         u = self.unit
 
         if s[0] == "-":
@@ -46,9 +77,9 @@ class Measurement (object):
         else:
             return "{0} &times; 10<sup>{1}</sup> {2}".format(s, e, u)
 
-    def toLaTeX(self, nsf = 3):
+    def toLaTeX(self, nsf=3):
         s = self.roundSignificand(nsf)
-        e = str( self.exponent)
+        e = str(self.exponent)
         u = self.unit
 
         if u in ["eV", "keV", "MeV", "GeV", "TeV"]:
@@ -61,51 +92,29 @@ class Measurement (object):
         else:
             return s + " \\times 10^{" + e + "} \\, " + u
 
-    def toDictionary(self, nsf = 3):
+    def toDictionary(self, nsf=3):
         return {
             "Significand":  self.roundSignificand(nsf),
             "Base": "10",
-            "Exponent": str( self.exponent),
-              "Unit": self.unit,
-            "Rounding": ( "{0}sf".format(nsf) if nsf > 0  else "none"),
-            "HTML": self.toHTML( nsf),
-            "LaTeX": self.toLaTeX( nsf)
-  }
-
-def orderOfMagnitude(n):
-    return int(math.floor(float(n.log10())))
-
-def roundToNSF(n, nsf = 3):
-    if n == Decimal("0") or nsf == 0:
-        return n
-
-    o = orderOfMagnitude(n)
-
-    m = n* Decimal("10") ** Decimal( -o + nsf - 1)
-    m = round(m, 0)
-    m = m* Decimal("10") ** Decimal( o- nsf + 1)
-
-    return m
-
+            "Exponent": str(self.exponent),
+            "Unit": self.unit,
+            "Rounding": ("{0}sf".format(nsf) if nsf > 0 else "none"),
+            "HTML": self.toHTML(nsf),
+            "LaTeX": self.toLaTeX(nsf)
+        }
 
 
 class Mass (object):
-    def __init__(self):
-        self.number = Decimal("0")
-        self.unit =""
+    def __init__(self, number, unit):
+        self.number = number
+        self.unit = unit
 
     @staticmethod
     def fromText(text):
-
         m = re.match(r"\s*[0]+\s*", text)
 
         if m:
-            mass = Mass()
-
-            mass.number = Decimal("0")
-            mass.unit = "kg"
-
-            return mass
+            return Mass(zero, "kg")
 
         m = re.match(r"\s*([+-]?\d+(\.\d+)?)\s*(\\times|\*)\s*10\^\{([+-]?\d+)\}\s*(g|kg|u|eV|keV|MeV|GeV|TeV)\s*", text)
 
@@ -114,12 +123,7 @@ class Mass (object):
             e = m.group(4)
             u = m.group(5)
 
-            mass = Mass()
-
-            mass.number = Decimal(s) * Decimal("10") ** Decimal(e)
-            mass.unit = u
-
-            return mass
+            return Mass(Decimal(s) * e1 ** Decimal(e), u)
 
         return None
 
@@ -127,63 +131,48 @@ class Mass (object):
         if self.unit == "kg":
             return self
         elif self.unit in ["eV", "keV", "MeV", "GeV", "TeV"]:
-            n = self.number * Decimal("1.78266192") * Decimal("10") ** Decimal("-36")
+            n = self.number * evcc
 
             if self.unit == "keV":
-                n = n *( Decimal("10") **Decimal("3"))
+                n = n * e3
             elif self.unit == "MeV":
-                n = n *( Decimal("10") **Decimal("6"))
+                n = n * e6
             elif self.unit == "GeV":
-                n = n *( Decimal("10") **Decimal("9"))
+                n = n * e9
             elif self.unit == "TeV":
-                n = n *( Decimal("10") **Decimal("10"))
+                n = n * e12
 
-            mass = Mass()
-
-            mass.number = n
-            mass.unit = "kg"
-
-            return mass
+            return Mass(n, "kg")
 
     def toev(self):
-        if self.unit in  ["eV", "keV", "MeV", "GeV", "TeV"]:
+        if self.unit in ["eV", "keV", "MeV", "GeV", "TeV"]:
             return self
 
         mkg = self.tokg().number
 
-        if mkg == Decimal(0):
-            mass = Mass()
+        if mkg == zero:
+            return Mass(mkg, "eV")
 
-            mass.number = mkg
-            mass.unit = "eV"
-
-            return mass
-
-        mev = mkg / ( Decimal("1.78266192") * Decimal(10) ** Decimal(-36))
+        mev = mkg / evcc
         o = orderOfMagnitude(mev)
 
         if o < 3:
             n = mev
             u = "eV"
         elif o >= 3 and o < 6:
-            n = mev / Decimal(10**3)
+            n = mev / e3
             u = "keV"
         elif o >= 6 and o < 9:
-            n = mev / Decimal(10**6)
+            n = mev / e6
             u = "MeV"
         elif o >= 9 and o < 12:
-            n = mev / Decimal(10**9)
+            n = mev / e9
             u = "GeV"
         elif o >= 12:
-            n = mev / Decimal(10**12)
+            n = mev / e12
             u = "TeV"
 
-        mass = Mass()
-
-        mass.number = n
-        mass.unit = u
-
-        return mass
+        return Mass(n, u)
 
     def tou(self):
         if self.unit == "u":
@@ -191,56 +180,37 @@ class Mass (object):
 
         mkg = self.tokg().number
 
-        if mkg == Decimal(0):
-            mass = Mass()
+        if mkg == zero:
+            return Mass(mkg, "u")
 
-            mass.number = mkg
-            mass.unit = "u"
+        mu = mkg / (Decimal("1.66053906660") * Decimal("10") ** Decimal("-27"))
 
-            return mass
-
-        mu = mkg / (  Decimal("1.66053906660") * Decimal(10) ** Decimal(-27))
-     
-        mass = Mass()
-
-        mass.number = mu
-        mass.unit = "u"
-
-        return mass
+        return Mass(mu, "u")
 
     def toMeasurement(self):
         s = self.number
 
-        if s == Decimal("0"):
-            m = Measurement()
-
-            m.significand = s
-            m.exponent = Decimal("0")
-            m.unit = self.unit
-
-            return m
-        
+        if s == zero:
+            return Measurement(s, zero, self.unit)
         else:
             o = orderOfMagnitude(s)
-            e = Decimal("0")
+            e = zero
 
             if o > 3 or o < -1:
-                s = s / ( Decimal("10") ** Decimal(o))
+                s = s / powerOf10(o)
                 e = o
 
-            m = Measurement()
-
-            m.significand = s
-            m.exponent = e
-            m.unit = self.unit
-
-            return m
+            return Measurement(s, e, self.unit)
 
     def toDictionary(self):
-        return [ self.tokg().toMeasurement().toDictionary(),
-        self.toev().toMeasurement().toDictionary(),
-        self.tou().toMeasurement().toDictionary(),
-        ]
+        return [self.tokg().toMeasurement().toDictionary(0),
+                self.tokg().toMeasurement().toDictionary(3),
+                self.toev().toMeasurement().toDictionary(0),
+                self.toev().toMeasurement().toDictionary(3),
+                self.tou().toMeasurement().toDictionary(0),
+                self.tou().toMeasurement().toDictionary(3),
+                ]
+
 
 class Compiler (object):
 
@@ -249,7 +219,7 @@ class Compiler (object):
 
         for a in os.listdir("../data"):
             if a.endswith(".particle"):
-                filePaths.append( os.path.join("../data", a))
+                filePaths.append(os.path.join("../data", a))
 
         return filePaths
 
@@ -267,39 +237,39 @@ class Compiler (object):
             particle["Name"] = lines[1]
             particle["MainSymbol"] = lines[2]
             particle["Symbol"] = lines[3]
-            particle["Classes"] = [c.strip() for c in  lines[4].split(",")]
+            particle["Classes"] = [c.strip() for c in lines[4].split(",")]
             particle["Antiparticle"] = {}
             particle["Antiparticle"]["Reference"] = ""
 
             lines = lines[5:]
 
-            for line in lines:                   
+            for line in lines:
                 if line.startswith("other names:"):
                     particle["OtherNames"] = [n.strip() for n in line[12:].strip().split(",")]
 
                 if line.startswith("mass:"):
-                    particle["Mass"] = Mass.fromText( line[5:].strip()).toDictionary()
+                    particle["Mass"] = Mass.fromText(line[5:].strip()).toDictionary()
 
                 if line.startswith("relative charge:"):
                     particle["RelativeCharge"] = line[16:].strip()
 
                     c = particle["RelativeCharge"]
-                    f=0
+                    f = 0
 
                     if c == "+1":
-                        f=1
+                        f = 1
                     if c == "+2/3":
-                        f=2/3
+                        f = 2/3
                     if c == "+1/3":
-                        f=1/3
+                        f = 1/3
                     if c == "0":
-                        f=0
+                        f = 0
                     if c == "-1/3":
-                        f=-1/3
+                        f = -1/3
                     if c == "-2/3":
-                        f=-2/3
+                        f = -2/3
                     if c == "-1":
-                        f=-1
+                        f = -1
 
                     charge = Decimal(f) * Decimal("1.602176634e-19")
 
@@ -307,20 +277,20 @@ class Compiler (object):
                         o = 0
                         s = 0
                     else:
-                        o =  int( math.floor(float( abs( charge).log10())))
+                        o = int(math.floor(float(abs(charge).log10())))
                         s = charge * Decimal(10) ** Decimal(-o)
 
-                    particle["Charge"] = {"Significand": str( s), "Exponent": o, "UnitClass":"C", "UnitsLaTeX":"C", "UnitsHTML": "C"}
+                    particle["Charge"] = {"Significand": str(s), "Exponent": o, "UnitClass": "C", "UnitsLaTeX": "C", "UnitsHTML": "C"}
 
                 if line.startswith("spin:"):
                     particle["Spin"] = line[5:].strip()
-            
+
                 if line.startswith("magnetic moment:"):
                     particle["MagneticMoment"] = line[16:].strip()
-        
+
                 if line.startswith("antiparticle:"):
                     particle["Antiparticle"]["Reference"] = line[13:].strip()
-    
+
                 if line.startswith("mean lifetime:"):
                     particle["MeanLifetime"] = line[14:].strip()
 
@@ -335,10 +305,10 @@ class Compiler (object):
 
                 if line.startswith("discovered by:"):
                     particle["DiscoveredBy"] = line[14:].strip()
-                    
+
                 if line.startswith("wikipedia:"):
                     particle["WikipediaURL"] = line[10:].strip()
-                
+
             return particle
 
     def convertLaTeXToHTML(self, latex):
@@ -365,9 +335,8 @@ class Compiler (object):
                 particle["Antiparticle"]["Name"] = antiparticles[0]["Name"]
                 particle["Antiparticle"]["URLReference"] = antiparticles[0]["URLReference"]
             else:
-                particle["Antiparticle"]["Name"] =""
+                particle["Antiparticle"]["Name"] = ""
                 particle["Antiparticle"]["URLReference"] = ""
-
 
         data = {}
 
@@ -380,11 +349,11 @@ class Compiler (object):
                 return str(o)
             raise TypeError()
 
-        with open("../data/Compiled.json", "w") as  fileObject:
-            json.dump(data, fileObject, indent = 4, default=decimal_default)
+        with open("../data/Compiled.json", "w") as fileObject:
+            json.dump(data, fileObject, indent=4, default=decimal_default)
 
-        with open("../web/particles.json", "w") as  fileObject:
-            json.dump(data, fileObject, indent = 4, default=decimal_default)
+        with open("../web/particles.json", "w") as fileObject:
+            json.dump(data, fileObject, indent=4, default=decimal_default)
 
 
 if __name__ == "__main__":
